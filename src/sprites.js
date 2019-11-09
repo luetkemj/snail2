4a0e35b9d47b4d65e6423b7287ce9c3200919c93
luetkemj <luetkemj@gmail.com>
2019-11-08 19:07:55 -0600

    set charset
    else prod chokes on unicode chars :grimacing:

--- src/sprites.js
+++ src/sprites.js
@@ -1,8 +1,7 @@
 import Cell from "overprint/overprint/cell";
 
 export default {
-  GRASS: Cell("·", "#37CC63"),
   PLAYER: Cell("@", "#DAA520"),
   WALL: Cell("#", "#AAA"),
-  FLOOR: Cell("·", "#AAA")
+  FLOOR: Cell("•", "#AAA")
 };
